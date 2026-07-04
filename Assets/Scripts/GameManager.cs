2349472b0e911f5eae378584a03c29d1ddf51a84
Hoon Jeong <iamhoonjeong@gmail.com>
2026-07-03 20:24:07 -0700

    Created game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    [Header("Player")]
+    [SerializeField] private GameObject playerPrefab;
+    [SerializeField] private Transform respawnPoint;
+    [SerializeField] private float respawnDelay;
+    public Player player;
+
+    [Header("Fruits Management")]
+    public bool fruitsHaveRandomLook;
+    public int fruitsCollected;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void RespawnPlayer() => StartCoroutine(RespawnCoroutine());
+
+    private IEnumerator RespawnCoroutine()
+    {
+        yield return new WaitForSeconds(respawnDelay);
+
+        GameObject newPlayer = Instantiate(playerPrefab, respawnPoint.position, Quaternion.identity);
+        player = newPlayer.GetComponent<Player>();
+    }
+
+    public void AddFruit() => fruitsCollected++;
+    public bool FruitsHaveRandomLook() => fruitsHaveRandomLook;
+}
