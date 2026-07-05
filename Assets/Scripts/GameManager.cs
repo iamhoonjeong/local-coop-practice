f5e3945d865e6128205ff0294621826360c9d8ee
Hoon Jeong <iamhoonjeong@gmail.com>
2026-07-04 19:25:01 -0700

    Implemented Traps

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,6 +19,9 @@ public class GameManager : MonoBehaviour
     [Header("Checkpoints")]
     public bool canReactivate;
 
+    [Header("Traps")]
+    public GameObject arrowPrefab;
+
     private void Awake()
     {
         if (instance == null)
@@ -51,5 +54,19 @@ public class GameManager : MonoBehaviour
     }
 
     public void AddFruit() => fruitsCollected++;
+
     public bool FruitsHaveRandomLook() => fruitsAreRandom;
+
+    public void CreateObject(GameObject prefab, Transform target, float delay = 0)
+    {
+        StartCoroutine(CreateObjectCoroutine(prefab, target, delay));
+    }
+
+    private IEnumerator CreateObjectCoroutine(GameObject prefab, Transform target, float delay)
+    {
+        Vector3 newPosition = target.position;
+        yield return new WaitForSeconds(delay);
+
+        GameObject newObject = Instantiate(prefab, newPosition, Quaternion.identity);
+    }
 }
---
+++ Assets/Scripts/Traps/Trap_Arrow.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Trap_Arrow : Trap_Trampoline
+{
+    [Header("Additional info")]
+    [SerializeField] private float cooldown;
+    [SerializeField] private bool rotationRight;
+    [SerializeField] private float rotationSpeed = 120;
+    private int direction;
+    [Space]
+    [SerializeField] private float scaleUpSpeed = 10;
+    [SerializeField] private Vector3 targetScale;
+
+    private void Start()
+    {
+        transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+    }
+
+    private void Update()
+    {
+        HandleScaleUp();
+        HandleRotation();
+    }
+
+    private void HandleScaleUp()
+    {
+        if (transform.localScale.x < targetScale.x)
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, scaleUpSpeed * Time.deltaTime);
+    }
+
+    private void HandleRotation()
+    {
+        direction = rotationRight ? -1 : 1;
+        transform.Rotate(0, 0, rotationSpeed * direction * Time.deltaTime);
+    }
+
+    private void DestoryMe()
+    {
+        GameObject arrowPrefab = GameManager.instance.arrowPrefab;
+        GameManager.instance.CreateObject(arrowPrefab, transform, cooldown);
+
+        Destroy(gameObject);
+    }
+}
